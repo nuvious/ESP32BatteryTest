ce0bd8bb401e6b38d4a4efc38211d48c3a69d4bc
David Cheeseman <nuvious@gmail.com>
2021-04-12 09:33:44 -0400

    Added some more code comments

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 import os
 import datetime as dt
 
+# Create the flask app.
 app = Flask(__name__)
 api = Api(app)
 
@@ -20,17 +21,25 @@ class Esp32Test(Resource):
         Returns:
             dict: A hello-world rest response.
         """
+
+        # If t0.txt doesn't exits.
         if not os.path.exists('t0.txt'):
+            # Set target output file to t0.txt
             fname = 't0.txt'
         else:
+            # Otherwise set it to t1.txt
             fname = 't1.txt'
+        # Write timestamp to target file
         with open(fname, 'w') as f:
             f.write(str(dt.datetime.now()))
+        # Return simple hello-world rest response.
         return {'hello': 'world'}
 
 
+# Bind Esp32Test Resource to the "/" path.
 api.add_resource(Esp32Test, '/')
 
 
 if __name__ == "__main__":
+    # Run app on default 5000 port and bind to 0.0.0.0
     app.run(host='0.0.0.0', debug=True)
